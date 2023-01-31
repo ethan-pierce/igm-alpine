c11f67753b508693a03a660b6b2af3121ee7e48a
Ethan Pierce <ethan.pierce@colorado.edu>
2023-01-31 13:27:32 -0700

    Edit output variables in inverse.py

--- mendenhall/inverse/inverse.py
+++ mendenhall/inverse/inverse.py
@@ -33,8 +33,7 @@ glacier.config.opti_smooth_anisotropy_factor = 0.2
 
 # Save variables
 glacier.config.opti_vars_to_save = ['topg', 'usurf', 'thk', 'strflowctrl', 'arrhenius', 'slidingco', 
-                                    'velsurf_mag', 'velsurfobs_mag', 'divflux', 
-                                    'velbase_mag', 'uvelbase', 'vvelbase', 'wvelbase']
+                                    'velsurf_mag', 'velsurfobs_mag', 'divflux']
 
 # Initialize inverse model
 glacier.initialize()
