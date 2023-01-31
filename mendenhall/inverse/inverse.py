343ed2dd80e241c107d3efb2319b71dd0559954f
Ethan Pierce <ethan.pierce@colorado.edu>
2023-01-31 11:09:46 -0700

    Add regularization to the inverse model

--- mendenhall/inverse/inverse.py
+++ mendenhall/inverse/inverse.py
@@ -24,6 +24,13 @@ glacier.config.opti_velsurfobs_std = 5 # m/a
 glacier.config.opti_thkobs_std = 5 # m
 glacier.config.opti_usurfobs_std = 5 # m
 
+# Regularization parameters
+glacier.config.opti_regu_param_thk = 10
+glacier.config.opti_regu_param_strflowctrl = 1
+
+# Smoothing parameters
+glacier.config.opti_smooth_anisotropy_factor = 0.2
+
 # Initialize inverse model
 glacier.initialize()
 
