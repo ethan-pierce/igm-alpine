3035db5e55dd7d61b8ce4119898b7f91fa742fb3
Ethan Pierce <ethan.pierce@colorado.edu>
2023-03-06 11:38:47 -0700

    Set up integrated model run

---
+++ mendenhall/basal-sliding/basal.py
@@ -0,0 +1,87 @@
+import os
+USER = os.environ.get('USER')
+
+import sys
+sys.path.append('/projects/' + USER + '/igm/src')
+
+import numpy as np
+import shutil
+import tensorflow as tf
+from igm import Igm
+
+####################################
+# Section 1: Run the inverse model #
+####################################
+
+glacier = Igm()
+
+# Point to ice flow emulator 
+glacier.config.iceflow_model_lib_path = '/projects/' + USER + '/igm/model-lib/f15_cfsflow_GJ_22_a/50'
+
+# Point to observation file
+glacier.config.observation_file = './inputs/observation.nc'
+
+# Set control and cost variables
+glacier.config.opti_control = ['thk', 'strflowctrl', 'usurf']
+glacier.config.opti_cost = ['velsurf', 'thk', 'usurf', 'icemask']
+
+# Set regularization parameters
+glacier.config.opti_regu_param_thk = 10.0
+glacier.config.opti_regu_param_strflowctrl = 5.0
+
+# Set smoothing and convexity parameters
+glacier.config.opti_smooth_anisotropy_factor = 0.2
+glacier.config.opti_convexity_weight = 0.002
+
+# Set computational parameters
+glacier.config.opti_nbitmax = 2000
+glacier.config.opti_output_freq = 50
+glacier.config.opti_step_size = 0.001
+glacier.config.opti_init_zero_thk = False
+
+# Choose variables to save
+glacier.config.opti_vars_to_save = ['topg', 'usurf', 'thk', 'strflowctrl', 'arrhenius', 'slidingco', 
+                                    'velsurf_mag', 'velsurfobs_mag', 'divflux']
+
+# Initialize inverse model
+glacier.initialize()
+
+# Run the optimization routine
+with tf.device("/GPU:0"):
+    glacier.load_ncdf_data(glacier.config.observation_file)
+    glacier.initialize_fields()
+    glacier.optimize()
+
+glacier.print_all_comp_info()
+
+####################################
+# Section 2: Run the forward model #
+####################################
+
+# Move optimized geology file to inputs
+shutil.copyfile('./geology-optimized.nc', './inputs/geology.nc')
+
+# Configure a new instance of IGM
+glacier = Igm()
+
+# Point to ice flow emulator 
+glacier.config.iceflow_model_lib_path = '/projects/' + USER + '/igm/model-lib/f15_cfsflow_GJ_22_a/50'
+
+# Point to geology file
+glacier.config.geology_file = './inputs/geology.nc'
+
+# Configure the model
+glacier.config.usegpu = True
+glacier.config.vars_to_save = ["topg", "usurf", "thk", "velbar_mag", "velsurf_mag", "divflux", "uvelsurf", "vvelsurf", "uvelbase", "vvelbase"]
+
+# Initialize the model
+glacier.initialize()
+
+# Run the forward model
+with tf.device("/GPU:0"):
+    glacier.load_ncdf_data(glacier.config.geology_file)
+    glacier.initialize_fields()
+
+    glacier.update_iceflow()
+
+glacier.print_all_comp_info()
