d4369984312a7d9b54239b75d506ffdecfea3221
Ethan Pierce <ethan.pierce@colorado.edu>
2023-05-25 10:37:50 -0600

    Add bulk job script

---
+++ mendenhall/ice-thk-confidence/run-all-jobs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sbatch outputs/100/igm-experiments.slurm
+sbatch outputs/80/igm-experiments.slurm
+sbatch outputs/60/igm-experiments.slurm
+sbatch outputs/40/igm-experiments.slurm
+sbatch outputs/20/igm-experiments.slurm
