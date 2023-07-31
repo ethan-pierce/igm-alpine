13241bccd527496df6347cbbafc607905bd4ade6
Ethan Pierce <ethan.pierce@colorado.edu>
2023-07-31 13:25:54 -0600

    Add PINN output

---
+++ mendenhall/pinn/02/clean.sh
@@ -0,0 +1,10 @@
+rm -r __pycache__
+rm igm-run-parameters.txt
+rm optimize.nc
+rm geology-optimized.nc
+rm convergence.png
+rm slidingco.dat
+rm rms_std.dat
+rm costs.dat
+rm volume.dat
+rm tcompoptimize.dat
