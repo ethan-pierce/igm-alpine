af349d4cf35b60cec837c4f709847ffbeec76b3e
Ethan Pierce <ethan.pierce@colorado.edu>
2023-09-13 15:26:44 -0600

    Add results

---
+++ igm-2.0/results/slidingco/60000/clean.sh
@@ -0,0 +1,9 @@
+rm params_saved.json
+rm optimize.nc
+rm geology-optimized.nc
+rm convergence.png
+rm rms_std.dat
+rm costs.dat
+rm output.nc
+rm computational-statistics.txt
+rm computational-pie.png
---
+++ igm-2.0/results/thkregu/base/clean.sh
@@ -0,0 +1,8 @@
+rm params_saved.json
+rm optimize.nc
+rm geology-optimized.nc
+rm convergence.png
+rm rms_std.dat
+rm costs.dat
+rm output.nc
+rm velbar_mag*.png
