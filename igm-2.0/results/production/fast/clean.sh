cf0c75fdcc13082a30bd79a2dcb9ba2d5d3f5f24
Ethan Pierce <ethan.pierce@colorado.edu>
2023-10-05 16:03:19 -0600

    Add output from Blanca

--- igm-2.0/results/production/fast/clean.sh
+++ igm-2.0/results/production/fast/clean.sh
@@ -18,3 +18,13 @@ rm output.nc
 rm velbar_mag*.png
 rm computational-statistics.txt
 rm computational-pie.png
+rm params_saved.json
+rm optimize.nc
+rm geology-optimized.nc
+rm convergence.png
+rm rms_std.dat
+rm costs.dat
+rm output.nc
+rm velbar_mag*.png
+rm computational-statistics.txt
+rm computational-pie.png
